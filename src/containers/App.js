1bfa553d4275486df2fe19cdd22ffb3dc61632b5
Star <mail.starcreator@gmail.com>
2021-05-23 19:51:36 +0100

    make it https

--- src/containers/App.js
+++ src/containers/App.js
@@ -15,7 +15,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch("http://jsonplaceholder.typicode.com/users")
+    fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => {
         return response.json();
       })
